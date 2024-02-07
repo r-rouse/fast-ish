9ddb9eb6180ba2dd41191cd01eb20c7a5978a245
Randall Rouse <randall.g.rouse@gmail.com>
2024-02-06 17:01:24 -0800

    packages

--- fast-ish/src/components/NavBar.js
+++ fast-ish/src/components/NavBar.js
@@ -11,20 +11,20 @@ const NavBar = () => {
                 <a class="navbar-brand" href="#">
                     ~
                     ~
-                    </a>
+                    </a>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                                 Clothes
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                                <li><a class="dropdown-item" href="/clothes">run</a></li>
-                                <li><a class="dropdown-item" href="/clothes">hike</a></li>
-                                <li><a class="dropdown-item" href="/clothes">chill</a></li>
+                                <li><a class="dropdown-item" href="/clothes">run</a></li>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                <li><a class="dropdown-item" href="/clothes">hike</a></li>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                <li><a class="dropdown-item" href="/clothes">chill</a></li>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             </ul>
                         </li>
                     </ul>
@@ -36,10 +36,10 @@ const NavBar = () => {
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Hats
-                            </a>
+                            </a>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                                <li><a class="dropdown-item" href="/hats">performace</a></li>
-                                <li><a class="dropdown-item" href="/hats">fashio</a></li>
+                                <li><a class="dropdown-item" href="/hats">performace</a></li>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                                <li><a class="dropdown-item" href="/hats">fashio</a></li>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             </ul>
                         </li>
                     </ul>
@@ -53,7 +53,7 @@ const NavBar = () => {
                                 Not clothes
                             </a>
                             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                                <li><a class="dropdown-item" href="/stuff">stuff</a></li>
+                                <li><a class="dropdown-item" href="/stuff">stuff</a></li>{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                             </ul>
                         </li>
                     </ul>
